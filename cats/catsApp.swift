b36efadfef55459b488d11b11f6657aa26a75533
tsvbasa <tsvbasa7@gmail.com>
2022-11-17 10:04:45 -0500

    Initial Commit

---
+++ cats/catsApp.swift
@@ -0,0 +1,17 @@
+//
+//  catsApp.swift
+//  cats
+//
+//  Created by Veronica Tellez on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct catsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
